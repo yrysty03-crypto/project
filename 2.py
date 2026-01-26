bebadfcb225616f81fc233bed0b17d77c46a6521
yrysty03-crypto <yrysty03@gmail.com>
2026-01-26 22:21:10 +0500

    commit

---
+++ 2.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+print("This will work!")
+print('This will also work!')
+print("Hello World!", end=" ")
+print("I will print on the same line.")
