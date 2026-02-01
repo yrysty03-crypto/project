973de41b4f296b530d13fe3b4be08f45f91e8223
yrysty03-crypto <yrysty03@gmail.com>
2026-02-01 18:45:12 +0500

    commit

---
+++ practice02/boolean/boolean_operators.py
@@ -0,0 +1,33 @@
+a = 10
+b = 5
+
+if a > 0 and b > 0:
+    print("Both are positive")
+
+x = -3
+y = 7
+
+if x > 0 or y > 0:
+    print("At least one number is positive")
+
+is_raining = False
+
+if not is_raining:
+    print("You don't need an umbrella")
+
+age = 20
+has_id = True
+
+if age >= 18 and has_id:
+    print("Entry allowed")
+
+n = 8
+
+if n % 2 == 0 and n > 5:
+    print("Even and greater than 5")
+
+a = int(input())
+b = int(input())
+
+if a > 0 and b < 0:
+    print("a is positive and b is negative")
