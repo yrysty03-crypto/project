4f510d310fbd6d0a34513f8789c4b834833934ae
yrysty03-crypto <yrysty03@gmail.com>
2026-02-01 18:29:26 +0500

    commit

---
+++ practice02/boolean/boolean_comparison.py
@@ -0,0 +1,27 @@
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+
+x = 5
+
+print(x > 0 and x < 10)
+
+x = 5
+
+print(x < 5 or x > 10)
+
+x = 5
+
+print(not(x > 3 and x < 10))
