963550f86a6a2ae291b62f4343fa623d0098842d
yrysty03-crypto <yrysty03@gmail.com>
2026-02-02 22:00:12 +0500

    commit

---
+++ practice02/if_else/if_else.py
@@ -0,0 +1,44 @@
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+number = 7
+
+if number % 2 == 0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+username = "Emil"
+
+if len(username) > 0:
+  print(f"Welcome, {username}!")
+else:
+  print("Error: Username cannot be empty")
+
+a = 5
+b = 9
+
+if a > b:
+    print(a)
+else:
+    print(b)
+
+age = int(input())
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
