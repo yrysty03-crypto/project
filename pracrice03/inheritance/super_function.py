b7e6fe29d7cd3274ad3b62c9446b506bac9c8032
yrysty03-crypto <yrysty03@gmail.com>
2026-02-17 23:05:29 +0500

    suoer function file

---
+++ pracrice03/inheritance/super_function.py
@@ -0,0 +1,23 @@
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+
+class Student(Person):
+  def __init__(self, fname, lname):
+    super().__init__(fname, lname)
+    self.graduationyear = 2019
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
+
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+  def welcome(self):
+    print("Welcome", self.firstname, self.lastname, "to the class of", self.graduationyear)
