28331259b8d78358a978870977e0a52494e6e6ae
yrysty03-crypto <yrysty03@gmail.com>
2026-03-09 16:23:08 +0500

    write files

---
+++ practice06/file_handling/write_files.py
@@ -0,0 +1,16 @@
+with open("demofile.txt", "a") as f:
+  f.write("Now the file has more content!")
+
+#open and read the file after the appending:
+with open("demofile.txt") as f:
+  print(f.read())
+
+with open("demofile.txt", "w") as f:
+  f.write("Woops! I have deleted the content!")
+
+#open and read the file after the overwriting:
+with open("demofile.txt") as f:
+  print(f.read())
+
+f = open("myfile.txt", "x")
+
